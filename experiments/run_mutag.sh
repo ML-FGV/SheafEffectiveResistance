2025b8a7088ce739404c5f9c4706c9cec1d1da4c
André <andrerg02@gmail.com>
2025-09-07 18:57:43 -0300

    Add sweep files

---
+++ experiments/run_mutag.sh
@@ -0,0 +1,9 @@
+python run_graph_classification.py \
+    --dataset mutag \
+    --rewiring None \
+    --display True \
+    --layer_type Sheaf \
+    --d 5 \
+    --gnn_type GIN \
+    --gnn_layers 5 \
+    --num_trials 10
